49384e907053249e461e554d833d45304636bbb1
rinesh.km <rinesh.km@accionlabs.com>
2023-08-17 15:55:29 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace asp_core_docker_k8s
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+        public DateTime ? Datetime { get; set; } 
+    }
+}
